178afc5f37063e5da3c0d9b7310b916937d00e1a
amirvest <dusenbaev707@mail.ru>
2023-03-11 15:06:21 +0400

    init

---
+++ src/main/java/ru/amir/library/utils/PersonValidator.java
@@ -0,0 +1,33 @@
+package ru.amir.library.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import ru.amir.library.dao.PersonDAO;
+import ru.amir.library.models.Person;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDAO personDAO;
+    @Autowired
+    public PersonValidator(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        if (personDAO.getPersonFullName(person.getFullName()).isPresent()) {
+            errors.rejectValue("fullName", "", "Человек с таким ФИО уже существует");
+        }
+    }
+}
